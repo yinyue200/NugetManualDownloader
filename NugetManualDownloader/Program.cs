46dc84468caa60aaecb2b5f795c670ac0bb5a186
yinyue200 <yinyue200@outlook.com>
2017-08-29 23:44:40 +0800

    fix encoding problem

--- NugetManualDownloader/Program.cs
+++ NugetManualDownloader/Program.cs
@@ -11,6 +11,9 @@ namespace NugetManualDownloader
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+
             Console.WriteLine("Copyright © yinyue200.com 2017");
             if (args.Length == 0)
             {
